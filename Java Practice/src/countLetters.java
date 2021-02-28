b521ccaadf66b87bc854963eecdf2c9dcdf014a3
Yash Kohad <Yash Kohad@LAPTOP-14O3CTQ9>
2021-02-28 20:24:51 +0530

    Updated countLetters.java

--- Java Practice/src/countLetters.java
+++ Java Practice/src/countLetters.java
@@ -1,42 +1,40 @@
-
 public class countLetters
 {
-
 	public static void main(String[] args) 
 	{
-		String s="Pune Pune  Pune";
-		int p=0;
-		int u=0;
-		int n=0;
-		int e=0;
-		int oth=0;
+		String s="Pune Pune Pune";
+		int p_cnt=0;
+		int u_cnt=0;
+		int n_cnt=0;
+		int e_cnt=0;
+		int oth_cnt=0; 
 		int len=s.length();
 		for(int i=0;i<len;i++)
 		{
 			if(s.charAt(i)=='P')
 			{
-				p++;
+				p_cnt++;
 			}
 			else if(s.charAt(i)=='u')
 			{
-				u++;
+				u_cnt++;
 			}
 			
 			else if(s.charAt(i)=='n')
 			{
-				n++;
+				n_cnt++;
 			}
 			else if(s.charAt(i)=='e')
 			{
-				e++;
+				e_cnt++;
 			}
 			else
 			{
-				oth++;
+				oth_cnt++;
 			}
 		}
 		
-        System.out.println("P= "+p+" U= "+u+" N= "+n+" E= "+e+" Other= "+oth);
+        System.out.println("P= "+p_cnt+" U= "+u_cnt+" N= "+n_cnt+" E= "+e_cnt+" Other= "+oth_cnt);
 	}
 
 }
